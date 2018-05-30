16896f8b0c97e885008897806249fff342bf6436
sudhanshu patel <sudhanshuptl13@gmail.com>
2018-05-30 08:35:33 +0530

    One way link list Complete

--- C-Programming/One_Way_Link_List.c
+++ C-Programming/One_Way_Link_List.c
@@ -9,9 +9,14 @@ struct Node{
 typedef struct Node Node;
 // You can reassign name using typedef  also
 
+//declaring prototype of function i gonna declare
 Node *createNode(int data);
 void insertAtBeginning(Node **head ,int data);
 void print(Node *head);
+void insertAtEnd(Node **head, int data);
+void insertAtPosition(Node **head,int data,int position);
+void deleteFromBeginning(Node **head);
+void deleteFromEnd(Node **head);
 
 int main(){
     Node *head = NULL;
@@ -20,6 +25,22 @@ int main(){
     insertAtBeginning(&head,3);
     insertAtBeginning(&head,2);
     insertAtBeginning(&head,1);
+    print(head);
+
+    insertAtEnd(&head,6);
+    insertAtEnd(&head,7);
+    print(head);
+
+    insertAtPosition(&head,4,4);
+    insertAtPosition(&head,5,5);
+    print(head);
+
+    deleteFromBeginning(&head);
+    deleteFromBeginning(&head);
+    print(head);
+
+    deleteFromEnd(&head);
+    deleteFromEnd(&head);
     print(head);
 
     return 0;
@@ -48,6 +69,106 @@ void insertAtBeginning(Node **head ,int data){
         node->next = *head;
         *head = node;
     }
+}
+
+void insertAtEnd(Node **head, int data){
+
+    Node *ptr=*head;
+    Node *node = createNode(data); // create a new node
+
+    if(node == NULL){
+        printf("Memory Allocation Error\n");
+        return;
+    }
+    //if list is empty insert as first node
+    if(*head == NULL){
+        //Since head is already double pointer so just passing value of head means
+        // sending address of single pointer of head
+        insertAtBeginning(head, data);
+        return;
+    }
+
+    //go to last node
+    while(ptr->next != NULL){
+        ptr = ptr->next;
+    }
+
+    //now insert new node after ptr
+    ptr->next = node;
+    node->next = NULL;
+}
+
+void insertAtPosition(Node **head,int data,int position){
+    Node *ptr = *head ;
+    int count = 1;
+    // *node because it receive address of a new node
+    Node *node = createNode(data);
+    if(node == NULL){
+        printf("Memory Allocation Error\n");
+        return;
+    }
+
+    //if list is empty insert at beginning
+    if(*head == NULL){
+        //Since head is already double pointer so just passing value of head means
+        // sending address of single pointer of head
+        insertAtBeginning(head, data);
+        return;
+    }
+
+    // move pointer to position -1
+    //if position not available insert at end
+    while(ptr->next != NULL && count < position-1){
+        ptr = ptr->next;
+        count++;
+    }
+    // now insert after ptr
+    node->next = ptr->next;
+    ptr->next = node;
+
+}
+
+void deleteFromBeginning(Node **head){
+
+    Node *node = *head;
+    // move head to next and free node
+    //check if list is empty
+    if(*head == NULL ){
+        printf("List is Empty!\n");
+        return;
+    }
+
+    *head = node->next;
+    free(node);
+}
+
+void deleteFromEnd(Node **head){
+    Node *ptr = *head;
+    Node *last_node;
+
+    //check if list is empty
+    if(*head == NULL ){
+        printf("List is Empty!\n");
+        return;
+    }
+
+
+    // If only one node
+    if(ptr->next == NULL){
+        *head = NULL;
+        free(ptr);
+        return;
+    }
+
+    //find second last node
+    while(ptr->next->next != NULL){
+        ptr = ptr->next;
+    }
+
+    last_node = ptr->next;
+    ptr->next = NULL;
+    free(last_node);
+
 }
 
 void print(Node *head){
