cce76f032fa8cd61c6e9a4c2428365edf81a6297
sudhanshu.patel <sudhanshu.patel@adroll.com>
2018-12-26 14:27:21 +0530

    developed basic app in RectJS

---
+++ Javascript/ReactJs/weather/src/components/ZipForm.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class ZipForm extends React.Component{
+    render(){
+        return <div className='zip-form'>
+        <form>
+            <label  htmlFor="zipcode">Zip Code</label>
+            <input 
+                className="form-control"
+                type="input"
+                name = "zipcode" />
+            <button type="submit" className="btn btn-success">Get the forcast </button>
+        </form>
+        </div>
+    }
+}
+
+export default ZipForm;
