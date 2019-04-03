cb7aed00a688b431ceb64646bf38d863f35860ea
sudhanshuptl <sudhanshuptl13@gmail.com>
2019-04-03 18:39:07 +0530

    slice and array

---
+++ GoLang/GO-Coursera/array.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	// default value for array is automatically assignedthat will be equivalent to 0 in that data datatype
+	// fixed length array
+	var arr1 [5]int
+	fmt.Println("Default initialisation", arr1)
+
+	var arr2 [5]int = [5]int{1, 2, 3, 4, 5}
+	fmt.Println("direct assignment", arr2)
+
+	y := [7]int{2, 3, 4, 5, 6, 7, 8}
+	fmt.Println("implicit direct assignment", y)
+
+	// does not specify exact size
+	z := [...]int{2, 3, 4, 5, 6, 7, 8, 10, 10, 11}
+	fmt.Println("implicit direct assignment", z)
+
+	//loop over array
+	for i, v := range z {
+		fmt.Println("index: ", i, "value: ", v)
+	}
+}
