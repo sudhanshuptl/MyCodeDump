a3f3a516b7e03e23e607afc47efa75d7dba86c83
sudhanshuptl <sudhanshuptl13@gmail.com>
2020-01-23 14:03:37 +0530

    full working with auth and admin auth

--- Python/Flask/REST/full_flask/flask_server.py
+++ Python/Flask/REST/full_flask/flask_server.py
@@ -1,13 +1,15 @@
 from flask import Flask, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
-import uuid
+from flask_sqlalchemy import  SQLAlchemy
+import uuid, jwt, datetime
+from functools import  wraps
+
 
 app = Flask(__name__)
 
 app.config['secret_key'] = 'ThisISSECRET'
 app.config['SQLALCHEMY_DATABASE_URI']=\
-    'sqlite://///Users/iopexses/Documents/Github/Interview-Preparation/Schlumberger/rest_demo/todo.db'
+    'sqlite://///Users/iopexses/Documents/Github/MyCodeDump/Python/Flask/REST/full_flask/todo.db'
 
 # User terminal & open python3
 # from flask_server import db
@@ -32,7 +34,50 @@ class todo(db.Model):
     user_id = db.Column(db.Integer)
 
 
+def basic_auth_required(f):
+    @wraps(f)  # this will preserver metadata and documentation of decorated FUNCTION
+    def decorated(*args, **kwrgs):
+        token = request.args.get('token')
+        if not token:
+            return jsonify({'msg': 'authentication token is required'}), 405
+        try:
+            jwt.decode(token, app.config['secret_key'])
+        except jwt.DecodeError as e:
+            return jsonify({"msg": "unable to authenticate "}), 401
+        except jwt.ExpiredSignature as e:
+            return jsonify({"msg": "Token expired get the new one "}), 401
+        else:
+            return f(*args, **kwrgs)
+    return decorated
+
+
+def admin_permission_required(f):
+    @wraps(f)  # this will preserver metadata and documentation of decorated FUNCTION
+    def decorated(*args, **kwrgs):
+        token = request.args.get('token')
+        if not token:
+            return jsonify({'msg': 'authentication token is required'}), 405
+        try:
+            public_id = jwt.decode(token, app.config['secret_key']).get('public_id')
+            if not public_id:
+                return jsonify({'msg': 'You need admin access for this'}), 405
+
+            user = User.query.filter_by(public_id = public_id ).first()
+            if not user:
+                return jsonify({'err': 'User might be recently deleted '}), 405
+            if not user.admin:
+                return jsonify({'err': 'Admin access required for this action'}),405
+        except jwt.DecodeError as e:
+            return jsonify({"msg": "unable to authenticate "}), 401
+        except jwt.ExpiredSignature as e:
+            return jsonify({"msg": "Token expired get the new one "}), 401
+        else:
+            return f(*args, **kwrgs)
+
+    return decorated
+
 @app.route('/user', methods=['GET'])
+@basic_auth_required
 def get_all_user():
     users = User.query.all()
     try:
@@ -49,6 +94,7 @@ def get_all_user():
         return jsonify({'users' : result}), 200
 
 @app.route('/user/<public_id>', methods=['PUT'])
+@admin_permission_required
 def promote_user(public_id):
     try:
         user = User.query.filter_by(public_id=public_id).first()
@@ -86,6 +132,7 @@ def get_user(public_id):
         return jsonify(result), 200
 
 @app.route('/user/<public_id>', methods=['DELETE'])
+@admin_permission_required
 def delete_user(public_id):
     try:
         user = User.query.filter_by(public_id=public_id).first()
@@ -104,6 +151,7 @@ def delete_user(public_id):
 
 # http://127.0.0.1:5000/user
 @app.route('/user', methods=['POST'])
+@admin_permission_required
 def create_user():
     '''
     http://127.0.0.1:5000/user
@@ -132,6 +180,25 @@ def create_user():
 
     return jsonify({"msg": "New User created"}), 200
 
+
+@app.route('/login', methods=['POST'])
+def login():
+    auth = request.authorization
+
+    if auth and auth.username and auth.password:
+        user = User.query.filter_by(name=auth.username).first()
+        if not user:
+            return jsonify({'err': f'user = `{auth.username}` does not exist '}), 405
+        elif check_password_hash(user.password, auth.password):
+            # generate jwt token
+            token = jwt.encode(dict(public_id=user.public_id,
+                                    exp=datetime.datetime.utcnow()+datetime.timedelta(minutes=10)),
+                               app.config['secret_key'])
+            return jsonify({'token': token.decode('UTF-8')}), 200
+        else:
+            return jsonify({'msg': 'wrong password'}), 405
+    else:
+        return jsonify({'msg': 'Missing credentials'})
 
 
 if __name__ == '__main__':
